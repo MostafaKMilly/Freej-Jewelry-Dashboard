5d1f5a69859c7398890be25a10f9c151abb11e91
Mostafa Milly <57003058+MostafaKMilly@users.noreply.github.com>
2021-10-04 20:19:30 +0300

    add dealers componenet

---
+++ src/components/pages/Dealers.js
@@ -0,0 +1,13 @@
+import { Box } from "@mui/system";
+import React from "react";
+import DealersTable from "../Dealers/DealersTable";
+
+function Dealers(props) {
+  return (
+    <Box>
+      <DealersTable />
+    </Box>
+  );
+}
+
+export default Dealers;
