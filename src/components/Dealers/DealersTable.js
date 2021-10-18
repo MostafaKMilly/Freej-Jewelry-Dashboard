dc69fbefcd3e1d5d63d31ce5342832b959494935
mostafakmilly <mostafamilly6@gmail.com>
2021-10-18 23:00:18 +0300

    add react-table in dealer page

--- src/components/Dealers/DealersTable.js
+++ src/components/Dealers/DealersTable.js
@@ -1,67 +1,19 @@
 import React from "react";
-import faker from "faker";
 import { Box } from "@mui/system";
-import { DataGrid } from "@mui/x-data-grid";
-
-const rows = [];
-for (let i = 1; i <= 14; i++) {
-  rows[i] = {
-    id: i,
-    firstName: faker.name.firstName(),
-    lastName: faker.name.lastName(),
-    address: faker.address.cityName(),
-    amount: Math.floor(Math.random() * 120),
-  };
-}
-
-const columns = [
-  {
-    field: "id",
-    headerName: "ID",
-    width: 120,
-    headerClassName: "super-app-theme--header",
-    headerAlign: "center",
-    align: "center",
-  },
-  {
-    field: "fullName",
-    headerName: "Full name",
-    valueGetter: (params) =>
-      `${params.getValue(params.id, "firstName") || ""} ${
-        params.getValue(params.id, "lastName") || ""
-      }`,
-    headerClassName: "super-app-theme--header",
-  },
-  {
-    field: "address",
-    headerName: "Address",
-    headerClassName: "super-app-theme--header",
-  },
-  {
-    field: "amount",
-    headerName: "Amount",
-    headerClassName: "super-app-theme--header",
-  },
-];
+import { CssBaseline } from "@mui/material";
+import {
+  dealersTableData,
+  dealerTableColumns,
+} from "../../shared/DealersTableData";
+import EnhancedTable from "../UI/EnhancedTable";
 
 function DealersTable(props) {
+  const columns = dealerTableColumns;
+  const data = dealersTableData;
   return (
     <Box sx={{ width: "100%", height: "400px" }}>
-      <Box sx={{ display: "flex", height: "100%" }}>
-        <Box sx={{ flexGrow: 1 }}>
-          <DataGrid
-            rows={rows}
-            columns={columns}
-            pageSize={5}
-            rowsPerPageOptions={[5]}
-            checkboxSelection
-            disableColumnMenu
-            style={{
-              border: "none",
-            }}
-          />
-        </Box>
-      </Box>
+      <CssBaseline />
+      <EnhancedTable columns={columns} data={data} />
     </Box>
   );
 }
---
+++ src/components/UI/GlobalFilter.js
@@ -0,0 +1,34 @@
+import { TextField, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import CustomTextField from "./CustomTextField";
+
+function GlobalFilter({ filter, setFilter }) {
+  return (
+    <Box
+      display="flex"
+      flexDirection="row"
+      alignItems="baseline"
+      sx={{
+        mb: 3,
+        mt: {
+          xs: 2,
+          md: 1,
+        },
+      }}
+    >
+      <Typography variant="body1" color="textSecondary" sx={{ mr: 2 }}>
+        بحث :
+      </Typography>
+      <CustomTextField
+        size="small"
+        variant="outlined"
+        value={filter || ""}
+        onChange={(e) => setFilter(e.target.value)}
+        sx={{ flexGrow: 0.08 }}
+      ></CustomTextField>
+    </Box>
+  );
+}
+
+export default GlobalFilter;
