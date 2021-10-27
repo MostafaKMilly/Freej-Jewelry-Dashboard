61e76bf70fd8ff57b463c3bee37c3c5a62e72e97
mostafakmilly <mostafamilly6@gmail.com>
2021-10-28 02:57:34 +0300

    edit addReceipts table headers

--- src/components/Receipts/ReceiptsTable.js
+++ src/components/Receipts/ReceiptsTable.js
@@ -1,15 +1,27 @@
 import React from "react";
 import { Box } from "@mui/system";
 import { CssBaseline } from "@mui/material";
-import {
-  receiptsTableData,
-  receiptsTableColumns,
-} from "../../shared/ReceiptsTableData";
 import EnhancedTable from "../UI/EnhancedTable";
 
-function ReceiptsTable(props) {
-  const columns = receiptsTableColumns;
-  const data = receiptsTableData;
+function ReceiptsTable({ data }) {
+  const columns = [
+    {
+      Header: "رقم الفاتورة",
+      accessor: "id",
+    },
+    {
+      Header: "تاريخ البيع",
+      accessor: "date",
+    },
+    {
+      Header: "مجموع الاجرة",
+      accessor: "totalRent",
+    },
+    {
+      Header: "القيمة الإجمالية",
+      accessor: "totalValue",
+    },
+  ];
   return (
     <Box sx={{ width: "100%", height: "400px" }}>
       <CssBaseline />
